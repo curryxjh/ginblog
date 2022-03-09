09675312921a1d1368c1e372098101127c4e796b
curryxjh <2648108535@qq.com>
2022-03-09 13:00:25 +0800

    接口

--- routes/router.go
+++ routes/router.go
@@ -1,9 +1,9 @@
 package routes
 
 import (
+	v1 "ginbolg/api/v1"
 	"ginbolg/utils"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 //路由入口文件
@@ -14,11 +14,15 @@ func InitRouter() {
 
 	router := r.Group("api/v1")
 	{
-		router.GET("Hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "ok",
-			})
-		})
+		//User模块的路由接口
+		router.POST("user/add", v1.AddUser)
+		router.GET("users", v1.GetUsers)
+		router.PUT("user/:id", v1.EditUser)
+		router.DELETE("user/:id", v1.DeleteUser)
+		//分类模块的路由接口
+
+		//文章模块的路由接口
+
 	}
 	r.Run(utils.HttpPort)
 }
