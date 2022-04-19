6648a2239cbdc05467d20e64df5878871547fc40
curryxjh <2648108535@qq.com>
2022-04-19 09:59:11 +0800

    相关错误定义

--- utils/errmsg/errmsg.go
+++ utils/errmsg/errmsg.go
@@ -13,6 +13,7 @@ const (
 	ERROR_TOKEN_TYPE_WRONG = 1007
 
 	//code = 2000 -->文章模块错误
+	ERROR_CATENAME_USED = 2001
 
 	//code = 3000 -->分类模块错误
 
@@ -28,6 +29,7 @@ var codeMsg = map[int]string{
 	ERROR_TOKEN_RUNTIME:    "TOKEN已过期",
 	ERROR_TOKEN_WRONG:      "TOKEN不正确",
 	ERROR_TOKEN_TYPE_WRONG: "TOKEN格式错误",
+	ERROR_CATENAME_USED:    "该分类已存在",
 }
 
 func GetErrMsg(code int) string {
